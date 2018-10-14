c8cacfcda5a09bd7c60a74ce5009da99c8f1d338
posseggs <simon.possegger@edu.htl-villach.at>
2018-10-14 11:31:44 +0200

    11:31:44-LAPTOP-POSSE-Commit script now has a message

--- commit_onPress.bat
+++ commit_onPress.bat
@@ -1,9 +1,7 @@
 setlocal EnableDelayedExpansion
-for /L %%i in (1,0,2) do (
+
 set /p msg=Enter message:
 git add .
 git commit -am "%time:~0,8%-%COMPUTERNAME%-%msg%"
 git push
-timeout /T 1 > nul
 pause
-)
