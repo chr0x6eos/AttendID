b23582e1413e86549335468c5cbbeef247eddb8d
posseggs <simon.possegger@edu.htl-villach.at>
2018-11-05 11:58:11 +0100

    11:58:11-LAPTOP-POSSE-Changed script so it uses actual timestamp values

--- database_connection_tests/database_connection_raspberry.py
+++ database_connection_tests/database_connection_raspberry.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import mysql.connector
+from time import gmtime, strftime
+import random
 
 '''
 def getDBs(mycursor): #Return all dbs
@@ -36,7 +38,8 @@ try:
     
     #Insert into table
     sql_query = ("INSERT INTO AttendingStudents (TimeStamp,Class,AttendingStudents) VALUE (%s,%s,%s)")
-    sql_value = ("2018-11-05 11:31:00","4AHITN", 0)
+    #sql_value = ("2018-11-05 11:31:00","4AHITN", 0)
+    sql_value = (strftime("%Y-%m-%d %H:%M:%S"),"4AHITN",random.randint(1,16))
     mycursor.execute(sql_query,sql_value)
     #Commit changes
     myDB.commit()
