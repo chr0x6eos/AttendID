4a0212c977bcdccaa92cabe3e75065a1b17cb0c5
posseggs <posseggs@edu.htl-villach.at>
2018-11-19 15:26:06 +0100

    15:26:05-LAPTOP-POSSE-Updating repo

--- face_recognition/faces_train.py
+++ face_recognition/faces_train.py
@@ -1,12 +1,76 @@
+'''
+Code from:
+https://github.com/codingforentrepreneurs/Guides/blob/master/LICENSE
+The MIT License (MIT)
+
+Copyright (c) 2014 CodingForEntrepreneurs.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+import cv2
 import os
+import numpy as np
+from PIL import Image
+import pickle
 
-#Adding existing training data to list
-base_dir = os.path.dirname(os.path.abspath(__file__))
-image_dir = os.path.join(base_dir, "faces")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(BASE_DIR, "images")
 
-for root, dirs, files  in os.walk(image_dir):
-    for file in files:
-       if file.endswith("png") or file.endswith("jpg"):
-           path = os.path.join(root, file)
-           print(path) 
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
 
+current_id = 0
+label_ids = {}
+y_labels = []
+x_train = []
+
+for root, dirs, files in os.walk(image_dir):
+	for file in files:
+		if file.endswith("png") or file.endswith("jpg"):
+			path = os.path.join(root, file)
+			label = os.path.basename(root).replace(" ", "-").lower()
+			#print(label, path)
+			if not label in label_ids:
+				label_ids[label] = current_id
+				current_id += 1
+			id_ = label_ids[label]
+			#print(label_ids)
+			#y_labels.append(label) # some number
+			#x_train.append(path) # verify this image, turn into a NUMPY arrray, GRAY
+			pil_image = Image.open(path).convert("L") # grayscale
+			size = (550, 550)
+			final_image = pil_image.resize(size, Image.ANTIALIAS)
+			image_array = np.array(final_image, "uint8")
+			#print(image_array)
+			faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+
+			for (x,y,w,h) in faces:
+				roi = image_array[y:y+h, x:x+w]
+				x_train.append(roi)
+				y_labels.append(id_)
+
+
+#print(y_labels)
+#print(x_train)
+
+with open("pickles/face-labels.pickle", 'wb') as f:
+	pickle.dump(label_ids, f)
+
+recognizer.train(x_train, np.array(y_labels))
+recognizer.save("recognizers/face-trainner.yml")
--- face_recognition/save_faces_proto.py
+++ face_recognition/save_faces_proto.py
@@ -2,6 +2,7 @@ import cv2
 from time import sleep
 import sys
 import os
+import numpy
 
 #cascPath ="../classifiers/haarcascade_frontalface_default.xml"
 cascPath="haarcascade_frontalface_default.xml"
@@ -9,15 +10,31 @@ faceCascade = cv2.CascadeClassifier(cascPath)
 webcam_capture = cv2.VideoCapture(0)
 saved_faces = 0
 
-dirName = "faces/unknownFace"
+recognizer = cv2.face.LBPHFaceRecognizer_create()
 
+usrName = input ("Enter your name:")
+dirName = "faces/"+usrName
+action = ""
 if len(sys.argv) > 1:
-    dirName = "faces/" + sys.argv[1] 
+    action = sys.argv[1]
+
+#if len(sys.argv) > 1:
+#    dirName = "faces/" + sys.argv[1] 
 try:
     # Create target Directory
     os.mkdir(dirName)
 except FileExistsError:
     pass #Already exists
+
+def saveFaces (faces,saved_faces):
+    for (x,y,w,h) in faces:
+                #img_pos_gray = gray[y:y+h,x:x+w]
+                saved_faces += 1
+                               #square coordinates (ystart, yend + xstart, xend) of face
+                img_pos = frame[y:y+h,x:x+w]
+                img_item = (dirName + "/{0}.png".format(saved_faces))
+                cv2.imwrite(img_item, img_pos)
+                #Save 10 faces
 
 while True:
     #If no webcam is detected wait for 5 seconds
@@ -34,19 +51,21 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(gray,scaleFactor=1.2,minNeighbors=5,minSize=(30, 30)    )
 
-    #If a face is found save the face as an img
-    if len(faces) > 0:
-        if saved_faces != 10:
-            for (x,y,w,h) in faces:
-                #img_pos_gray = gray[y:y+h,x:x+w]
-                saved_faces += 1
-                               #square coordinates (ystart, yend + xstart, xend) of face
-                img_pos = frame[y:y+h,x:x+w]
-                img_item = (dirName + "/{0}.png".format(saved_faces))
-                cv2.imwrite(img_item, img_pos)
-                #Save 10 faces
-        else:
-            break
+    if action == "save":
+        #If a face is found save the face as an img
+        if len(faces) > 0:
+            if saved_faces != 10:
+                saveFaces(faces,saved_faces)
+            else:
+                break
+    elif action == "recognize":
+        for (x,y,w,h) in faces:
+            gray = gray[y:y+h,x:x+w]
+            id, conf = recognizer.predict(gray)
+            # confidence
+    else:
+        print("Unknown action")
+        break
     
      #Press q to exit programm
     if cv2.waitKey(1) & 0xFF == ord('q'):
