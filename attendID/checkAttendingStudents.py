b5d5d3c216c2abe041231170099a9aa576bf7b6b
posseggs <posseggs@edu.htl-villach.at>
2018-12-03 11:17:47 +0100

    11:17:47-LAPTOP-POSSE-changed db name

--- attendID/checkAttendingStudents.py
+++ attendID/checkAttendingStudents.py
@@ -23,7 +23,7 @@ class DB_Connection:
     def insert(self,timeValue,classValue='4AHITN',studentsValue=0):
         #Query for creating students
         try:
-            sql_query=("INSERT INTO AttendingStudents (TimeStamp,Class,AttendingStudents) VALUE (%s,%s,%s)") 
+            sql_query=("INSERT INTO AttendingStudents (ReadingTime,Class,AttendingStudents) VALUE (%s,%s,%s)") 
             sql_values=(timeValue,classValue,studentsValue) #Value needs to be in format time,class,students
             #Executes the query and writes into the database
             self.mycursor.execute(sql_query,sql_values)
