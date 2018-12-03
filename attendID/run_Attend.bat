5ee22f4e5d017c6eeb93faed23243529636cdfe0
posseggs <posseggs@edu.htl-villach.at>
2018-12-03 15:05:11 +0100

    15:05:11-LAPTOP-POSSE-added old script

--- attendID/run_Attend.bat
+++ attendID/run_Attend.bat
@@ -1,7 +1,6 @@
 @echo off
-set /p class=Enter Class:
-set /p students=Enter Students:
+
 set /p debug=Enter debug:
 
-python checkAttendingStudents.py %class% %students% %debug%
+python checkAttendingStudents.py 4ahitn 16 %debug%
 pause
