362b94b4f945dcbdf27f0604924d3895a62a1816
posseggs <posseggs@edu.htl-villach.at>
2018-12-10 09:39:11 +0100

    9:39:11-LAPTOP-POSSE-Changed path of img

--- attendID/checkAttendingStudents.py
+++ attendID/checkAttendingStudents.py
@@ -142,7 +142,8 @@ while True:
         debugMsg("Saving last computed img for debugging")
         for (x,y,w,h) in faces: #Drawing rectangle
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.imwrite("reading.png", frame)
+        path = os.path.abspath("/pics/reading.png")
+        cv2.imwrite(path, frame)
 
         #Calculating average
         debugMsg("Calculating average")
